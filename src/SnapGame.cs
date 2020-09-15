4c038b0941b746caef01ce9c43ae1dfa44f967c5
victorleezhesheng <36871691+victorleezhesheng@users.noreply.github.com>
2020-09-15 21:15:20 +0800

    slap.wav & slap1.wav copied

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -9,82 +9,102 @@ namespace CardGames
         public static void LoadResources()
         {
             Bitmap cards;
-            cards = SwinGame.LoadBitmapNamed ("Cards", "Cards.png");
-            SwinGame.BitmapSetCellDetails (cards, 82, 110, 13, 5, 53);      // set the cells in the bitmap to match the cards
-			SwinGame.LoadFontNamed ("GameFont", "Chunkfive.otf", 24);
-		}
+            cards = SwinGame.LoadBitmapNamed("Cards", "Cards.png");
+            SwinGame.BitmapSetCellDetails(cards, 82, 110, 13, 5, 53);      // set the cells in the bitmap to match the cards
+            SwinGame.LoadFontNamed("GameFont", "Chunkfive.otf", 24);
+        }
 
-		/// <summary>
-		/// Respond to the user input -- with requests affecting myGame
-		/// </summary>
-		/// <param name="myGame">The game object to update in response to events.</param>
-		private static void HandleUserInput(Snap myGame)
-		{
-			//Fetch the next batch of UI interaction
-			SwinGame.ProcessEvents();
+        /// <summary>
+        /// Respond to the user input -- with requests affecting myGame
+        /// </summary>
+        /// <param name="myGame">The game object to update in response to events.</param>
+        private static void HandleUserInput(Snap myGame)
+        {
+            //Fetch the next batch of UI interaction
+            SwinGame.ProcessEvents();
 
-			if (SwinGame.KeyTyped (KeyCode.vk_SPACE))
-			{
-				myGame.FlipNextCard ();
-				myGame.Start ();
-			}
-		}
+            if (SwinGame.KeyTyped(KeyCode.vk_SPACE))
+            {
+                myGame.FlipNextCard();
+                myGame.Start();
+            }
+            if (myGame.IsStarted)
+            {
+                if (SwinGame.KeyTyped(KeyCode.vk_LSHIFT) &&
+                SwinGame.KeyTyped(KeyCode.vk_RSHIFT))
+                {
+                    //TODO: add sound effects
+                    //Loading Sound Effect
+                    SwinGame.LoadSoundEffectNamed("Slap", "slap1.wav");
+                    //Play Sound Effect
+                    SwinGame.PlaySoundEffect("Slap");
+                }
+                else if (SwinGame.KeyTyped(KeyCode.vk_LSHIFT))
+                {
+                    myGame.PlayerHit(0);
+                }
+                else if (SwinGame.KeyTyped(KeyCode.vk_RSHIFT))
+                {
+                    myGame.PlayerHit(1);
+                }
+            }
+        }
 
-		/// <summary>
-		/// Draws the game to the Window.
-		/// </summary>
-		/// <param name="myGame">The details of the game -- mostly top card and scores.</param>
-		private static void DrawGame(Snap myGame)
-		{
-			SwinGame.DrawBitmap("cardsBoard.png", 0, 0);
+        /// <summary>
+        /// Draws the game to the Window.
+        /// </summary>
+        /// <param name="myGame">The details of the game -- mostly top card and scores.</param>
+        private static void DrawGame(Snap myGame)
+        {
+            SwinGame.DrawBitmap("cardsBoard.png", 0, 0);
 
-			// Draw the top card
-			Card top = myGame.TopCard;
-			if (top != null)
-			{
-				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.White,"GameFont", 0, 30);
-				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.White,"GameFont", 0, 30);
-				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.White,"GameFont", 0, 30);
-				SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex, 521, 153);
-			}
-			else
-			{
-				SwinGame.DrawText ("No card played yet...", Color.RoyalBlue, 0, 20);
-			}
+            // Draw the top card
+            Card top = myGame.TopCard;
+            if (top != null)
+            {
+                SwinGame.DrawText("Top Card is " + top.ToString(), Color.White, "GameFont", 0, 30);
+                SwinGame.DrawText("Player 1 score: " + myGame.Score(0), Color.White, "GameFont", 0, 30);
+                SwinGame.DrawText("Player 2 score: " + myGame.Score(1), Color.White, "GameFont", 0, 30);
+                SwinGame.DrawCell(SwinGame.BitmapNamed("Cards"), top.CardIndex, 521, 153);
+            }
+            else
+            {
+                SwinGame.DrawText("No card played yet...", Color.RoyalBlue, 0, 20);
+            }
 
-			// Draw the back of the cards... to represent the deck
-			SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), 52, 155, 153);
+            // Draw the back of the cards... to represent the deck
+            SwinGame.DrawCell(SwinGame.BitmapNamed("Cards"), 52, 155, 153);
 
-			//Draw onto the screen
-			SwinGame.RefreshScreen(60);
-		}
+            //Draw onto the screen
+            SwinGame.RefreshScreen(60);
+        }
 
-		/// <summary>
-		/// Updates the game -- it should flip the cards itself once started!
-		/// </summary>
-		/// <param name="myGame">The game to be updated...</param>
-		private static void UpdateGame(Snap myGame)
-		{
-			myGame.Update(); // just ask the game to do this...
-		}
+        /// <summary>
+        /// Updates the game -- it should flip the cards itself once started!
+        /// </summary>
+        /// <param name="myGame">The game to be updated...</param>
+        private static void UpdateGame(Snap myGame)
+        {
+            myGame.Update(); // just ask the game to do this...
+        }
 
         public static void Main()
         {
             //Open the game window
             SwinGame.OpenGraphicsWindow("Snap!", 860, 500);
 
-			//Load the card images and set their cell details
+            //Load the card images and set their cell details
             LoadResources();
-            
-			// Create the game!
-			Snap myGame = new Snap ();
+
+            // Create the game!
+            Snap myGame = new Snap();
 
             //Run the game loop
-            while(false == SwinGame.WindowCloseRequested())
+            while (false == SwinGame.WindowCloseRequested())
             {
-				HandleUserInput (myGame);
-				DrawGame (myGame);
-				UpdateGame (myGame);
+                HandleUserInput(myGame);
+                DrawGame(myGame);
+                UpdateGame(myGame);
             }
         }
     }
